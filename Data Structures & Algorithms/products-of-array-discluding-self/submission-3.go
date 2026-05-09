99636ad878d810590dea0297b2b2582f57564752
MANAB   PRATIM MAITY <79935184+manab-pr@users.noreply.github.com>
2026-05-09 23:15:08 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.go
@@ -0,0 +1,19 @@
+func productExceptSelf(nums []int) []int {
+  n := len(nums)
+  res :=make([]int,n)
+  prefix :=make([]int,n)
+  suffix :=make([]int,n)
+
+  prefix[0] , suffix[n-1] = 1 , 1
+
+  for i := 1 ; i < n ; i++ {
+    prefix[i] = nums[i-1] * prefix[i-1]
+  }
+  for i :=n-2; i >=0 ; i-- {
+    suffix[i] = nums[i+1]*suffix[i+1]
+  }
+  for i :=0 ; i < n ; i++ {
+    res[i] = prefix[i]*suffix[i]
+  }
+  return res
+}
